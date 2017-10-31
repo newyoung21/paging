a199bd3ed2459f6c9a8147bd411325f3eafe6c28
newyoung21 <854795800@qq.com>
2017-10-31 08:11:05 +0800

    add callBack

--- js/paging.js
+++ js/paging.js
@@ -4,16 +4,17 @@
 // total -> 总页数
 // show -> 显示多少页面 
 // select -> 分页列表容器
-function Paging(current,total,show,select){
-	this.init(current,total,show,select);
+function Paging(current,total,show,select,callback){
+	this.init(current,total,show,select,callback);
 }
  Paging.prototype = {
 	//初始化 
-	init: function(current,total,show,select){
+	init: function(current,total,show,select,callback){
 		this.current = current;
 		this.total = total;
 		this.show = show;
 		this.select = select;
+		this.callback = callback;
 		this.bind();
 	},
 	//构建分页列表
@@ -34,38 +35,50 @@ function Paging(current,total,show,select){
 			end = begin+this.show;
 			this.createLi(begin,end);
 		}
-		
 	},
 	//创建li元素添加到分页列表
 	createLi: function(begin,end){
+		var me = this;
 		//判断是否显示第一页
 		if(begin>1){
-			var first = $("<li><a href='#/1'>首页</a></li>");
+			var first = $("<li><a data-idx='1' href='javascript:;' id='tt'>首页</a></li>");
 			$(this.select).append(first);
 		}
 		//判断是否显示上一页
 		if(this.current>1){
-			var prve = $("<li><a href='#/"+(this.current-1)+"'>&laquo;</a></li>");
+			var prve = $("<li><a data-idx='"+(this.current-1)+"' href='javascript:;'>&laquo;</a></li>");
 			$(this.select).append(prve);
 		}
 		//遍历显示页	
 		for(var i = begin; i < end; i++){
 			if(i == this.current){
-				var liElemnet =$("<li class='active'><a href='#/"+i+"'>"+i+"</a></li>");
+				var liElemnet =$("<li class='active'><a data-idx='"+i+"' href='javascript:;'>"+i+"</a></li>");
 			}else{
-				var liElemnet =$("<li><a href='#/"+i+"'>"+i+"</a></li>");
+				var liElemnet =$("<li><a data-idx='"+i+"'' href='javascript:;'>"+i+"</a></li>");
 			}
 			$(this.select).append(liElemnet);
 		}
 		//判断是否显示下一页
 		if(this.current<this.total){
-			var next = $("<li><a href='#/"+(this.current+1)+"'>&raquo;</a></li>");
+			var next = $("<li><a data-idx='"+(this.current+1)+"' href='javascript:;'>&raquo;</a></li>");
 			$(this.select).append(next);
 		}
 		//判断是否显示尾页
 		if(end<=this.total){
-			var last = $("<li><a href='#/"+(this.total)+"'>尾页</a></li>");
+			var last = $("<li><a data-idx='"+this.total+"' href='javascript:;'>尾页</a></li>");
 			$(this.select).append(last);
 		}
+		this.Click();
+	},
+	Click: function(){
+		var me = this;
+		$(this.select).find('li a').on('click',function(){
+			var idx = $(this).attr('data-idx');
+			me.current = Number(idx);
+			$(me.select).empty();
+			me.bind();
+			me.callback(idx);
+		})
 	}
+	
 }
